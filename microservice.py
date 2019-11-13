7a422d550ba63a3fc98c5f058a26eb6c47205ebc
Michael Roberts <michael.roberts.jr217@gmail.com>
2019-11-13 00:49:47 -0500

    Added /api/buy and /api/sell route functionality

--- microservice.py
+++ microservice.py
@@ -1,18 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 import sqlalchemy as db
 import pymysql
 import jwt
 import http
 import json
+import requests
 
 app = Flask(__name__)
 
-app.config['TRADIER_BEARER'] = ''
-app.config['SECRET'] = ''
-app.config['DB_HOST'] = ''
-app.config['DB_USER'] = ''
-app.config['DB_PASS'] = ''
-app.config['DB_NAME'] = ''
+app.config['TRADIER_BEARER'] = 'uhzCQ8Lzm5Tx35faBndmsYmQgE4d'
+app.config['SECRET'] = 'XCAP05H6LoKvbRRa/QkqLNMI7cOHguaRyHzyg7n5qEkGjQmtBhz4SzYh4Fqwjyi3KJHlSXKPwVu2+bXr6CtpgQ=='
+app.config['DB_HOST'] = '35.202.171.233'
+app.config['DB_USER'] = 'admin'
+app.config['DB_PASS'] = 'team6adminpass'
+app.config['DB_NAME'] = 'transactions'
 
 engine = db.create_engine('mysql+pymysql://' + app.config['DB_USER'] + ':' + app.config['DB_PASS'] + '@' + app.config['DB_HOST'] + '/' + app.config['DB_NAME'], pool_pre_ping=True)
 app.config['DB_CONN'] = engine.connect()
@@ -32,17 +33,55 @@ def quotes():
     except http.client.HTTPException as e:
         print('Quote Request Failed')
 
-    return quote, 200
+    res = make_response()
+    res.headers['quote'] = quote
+    return res
 
-@app.route('/api/buy')
+@app.route('/api/buy', methods=['POST'])
 def buy():
+    auth = request.headers.get('auth')
+    quantity = request.headers.get('quantity')
 
-    return 'Not Built', 200
+    try:
+        decoded = jwt.decode(auth, app.config['SECRET'], algorithm='HS256')
+        uid = decoded['uid']
+        username = decoded['username']
+        email = decoded['email']
 
-@app.route('/api/sell')
+        res = requests.get('http://localhost:5001/api/quotes')
+        price = json.loads(res.headers['quote'].replace('\'', '\"'))['quotes']['quote']['last']
+
+        sql = 'INSERT INTO buy_sell(b_type, username, t_account, price, quantity) VALUES(\'buy\', \'' + username + '\', \'' + email + '\', \'' + str(price) + '\', \'' + quantity + '\')'
+        app.config['DB_CONN'].execute(sql)
+        
+    except jwt.ExpiredSignatureError:
+        print('Expired Token')
+    except jwt.DecodeError:
+        print('Invalid Token')
+    return 'Working on it', 200
+
+@app.route('/api/sell', methods=['POST'])
 def sell():
+    auth = request.headers.get('auth')
+    quantity = request.headers.get('quantity')
 
-    return 'Not Built', 200
+    try:
+        decoded = jwt.decode(auth, app.config['SECRET'], algorithm='HS256')
+        uid = decoded['uid']
+        username = decoded['username']
+        email = decoded['email']
+
+        res = requests.get('http://localhost:5001/api/quotes')
+        price = json.loads(res.headers['quote'].replace('\'', '\"'))['quotes']['quote']['last']
+
+        sql = 'INSERT INTO buy_sell(b_type, username, t_account, price, quantity) VALUES(\'sell\', \'' + username + '\', \'' + email + '\', \'' + str(price) + '\', \'' + quantity + '\')'
+        app.config['DB_CONN'].execute(sql)
+        
+    except jwt.ExpiredSignatureError:
+        print('Expired Token')
+    except jwt.DecodeError:
+        print('Invalid Token')
+    return 'Working on it', 200
 
 @app.route('/api/transactions', methods=['GET'])
 def transactions():
