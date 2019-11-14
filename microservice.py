4ca9b21fdc28b84bec6508ae17f8a630ff861a62
Michael Roberts <michael.roberts.jr217@gmail.com>
2019-11-14 18:30:25 -0500

    Added acceptance/unit tests, functionalized app further

--- microservice.py
+++ microservice.py
@@ -15,59 +15,52 @@ app.config['DB_USER'] = ''
 app.config['DB_PASS'] = ''
 app.config['DB_NAME'] = ''
 
-engine = db.create_engine('mysql+pymysql://' + app.config['DB_USER'] + ':' + app.config['DB_PASS'] + '@' + app.config['DB_HOST'] + '/' + app.config['DB_NAME'], pool_pre_ping=True)
-app.config['DB_CONN'] = engine.connect()
+app.config['DB_ENGINE'] = db.create_engine('mysql+pymysql://' + app.config['DB_USER'] + ':' + app.config['DB_PASS'] + '@' + app.config['DB_HOST'] + '/' + app.config['DB_NAME'], pool_pre_ping=True)
 
 def authenticate(auth):
-    output = {
-        'uid' : '',
-        'username' : '',
-        'email' : ''
-    }
     try:
         decoded = jwt.decode(auth, app.config['SECRET'], algorithm='HS256')
-
+        output = {}
         output['uid'] = decoded['uid']
         output['username'] = decoded['username']
         output['email'] = decoded['email']
         
     except jwt.ExpiredSignatureError:
-        print('Expired Token')
+        output = 'Access token is missing or invalid'
     except jwt.DecodeError:
-        print('Invalid Token')
+        output = 'Access token is missing or invalid'
     return output
 
-def save_to_db(b_type, name, acc, price, amt):
-
+def save_to_db(b_type, name, acc, price, amt, inventory):
     if name != '' and acc != '' and float(price) > 0 and int(amt) > 0:
-        sql = ''
         if(b_type == 'BUY'):
-            inventory = get_stock_inventory()
             if inventory - int(amt) > 0:
                 sql = 'INSERT INTO buy_sell(b_type, username, t_account, price, quantity) VALUES(\'SELL\', \'admin\', \'admin@obs.com\', \'' + str(price) + '\', \'' + str(amt) + '\'),'
-                sql += '(\'' + b_type + '\', \'' + name + '\', \'' + acc + '\', \'' + str(price) + '\', \'' + str(amt) + '\')'
-                app.config['DB_CONN'].execute(sql)
+                sql += '(\'' + b_type + '\', \'' + name + '\', \'' + acc + '\', \'' + str(price) + '\', \'' + str(amt) + '\');'
+                buy = query_db(sql)
+                return 'Bought from stock inventory'
             else:
                 required = int(amt) - inventory + 100
                 sql = 'INSERT INTO buy_sell(b_type, username, t_account, price, quantity) VALUES(\'BUY\', \'admin\', \'admin@obs.com\', \'' + str(price) + '\', \'' + str(required) + '\'),'
                 sql += '(\'SELL\', \'admin\', \'admin@obs.com\', \'' + str(price) + '\', \'' + str(amt) + '\'),'
-                sql += '(\'' + b_type + '\', \'' + name + '\', \'' + acc + '\', \'' + str(price) + '\', \'' + str(amt) + '\')'
-                app.config['DB_CONN'].execute(sql)
+                sql += '(\'' + b_type + '\', \'' + name + '\', \'' + acc + '\', \'' + str(price) + '\', \'' + str(amt) + '\');'
+                buy = query_db(sql)
+                return 'Stock inventory overdrawn, inventory bought needed amt plus 100 and completed the buy'
         elif(b_type == 'SELL'):
             sql = 'INSERT INTO buy_sell(b_type, username, t_account, price, quantity) VALUES(\'BUY\', \'admin\', \'admin@obs.com\', \'' + str(price) + '\', \'' + str(amt) + '\'),'
-            sql += '(\'' + b_type + '\', \'' + name + '\', \'' + acc + '\', \'' + str(price) + '\', \'' + str(amt) + '\')'
-            app.config['DB_CONN'].execute(sql)
-        return 1
+            sql += '(\'' + b_type + '\', \'' + name + '\', \'' + acc + '\', \'' + str(price) + '\', \'' + str(amt) + '\');'
+            sell = query_db(sql)
+            return 'Sold to stock inventory'
     else:
-        return 0
+        return 'Invalid order amount or quoted price'
 
 def get_stock_inventory():
 
     bought_query = 'SELECT sum(quantity) AS bought FROM buy_sell WHERE (username = \'admin\' AND b_type = \'BUY\') OR (username != \'admin\' AND b_type = \'SELL\')'
-    bought = app.config['DB_CONN'].execute(bought_query).fetchall()[0][0]
+    bought = query_db(bought_query)[0][0]
 
     sold_query = 'SELECT sum(quantity) AS sold FROM buy_sell WHERE username != \'admin\' AND b_type = \'BUY\''
-    sold = app.config['DB_CONN'].execute(sold_query).fetchall()[0][0]
+    sold = query_db(sold_query)[0][0]
 
     if sold != None:
         remaining = int(bought) - int(sold)
@@ -90,6 +83,20 @@ def form_buy_sell_response(b_type, name, acc, price, amt):
     except json.JSONDecodeError:
         print('Error while forming JSON response for transaction')
     return transaction
+
+def get_delayed_price():
+
+    res = quotes() 
+    new_res = res[0]
+    delayed = new_res['quotes']['quote']['last']
+
+    return delayed
+
+def query_db(sql):
+    res = app.config['DB_ENGINE'].connect().execute(sql)
+    if 'SELECT' in sql:
+        res = res.fetchall()
+    return res
 
 #utilize get requests for quotes (using auth header) and transcations (using auth header with admin user)
 #and post requests for buy and sell, returning data for the sale made/failed
@@ -115,15 +122,14 @@ def buy():
 
     user_data = authenticate(auth)
     
-    res = requests.get('http://localhost:5001/api/quotes')
-    new_res = res.json()
-    price = new_res['quotes']['quote']['last']
+    price = get_delayed_price()
 
-    saved = save_to_db('BUY', user_data['username'], user_data['email'], price, quantity)
-
-    buy = form_buy_sell_response('BUY', user_data['username'], user_data['email'], price, quantity)
-
-    return buy, 200
+    if type(user_data) == type({}):
+        saved = save_to_db('BUY', user_data['username'], user_data['email'], price, quantity, get_stock_inventory())
+        buy = form_buy_sell_response('BUY', user_data['username'], user_data['email'], price, quantity)
+        return buy, 200
+    else:
+        return user_data, 401
 
 @app.route('/api/sell', methods=['POST'])
 def sell():
@@ -132,15 +138,14 @@ def sell():
 
     user_data = authenticate(auth)
 
-    res = requests.get('http://localhost:5001/api/quotes')
-    new_res = res.json()
-    price = new_res['quotes']['quote']['last']
+    price = get_delayed_price()
 
-    saved = save_to_db('SELL', user_data['username'], user_data['email'], price, quantity)
-
-    sell = form_buy_sell_response('BUY', user_data['username'], user_data['email'], price, quantity)
-        
-    return sell, 200
+    if type(user_data) == type({}):
+        saved = save_to_db('SELL', user_data['username'], user_data['email'], price, quantity, get_stock_inventory())
+        sell = form_buy_sell_response('SELL', user_data['username'], user_data['email'], price, quantity)
+        return sell, 200
+    else:
+        return user_data, 401
 
 @app.route('/api/transactions', methods=['GET'])
 def transactions():
@@ -148,22 +153,23 @@ def transactions():
     transactions = json.loads('{}')
 
     user_data = authenticate(auth)
+    if type(user_data) == type({}):
+        if user_data['email'] == 'admin@obs.com':
 
-    if user_data['email'] == 'admin@obs.com':
+            sql = 'SELECT JSON_OBJECT(\'bid\', bid, \'b_type\', b_type, \'username\', username, \'t_account\', t_account, \'price\', price, \'quantity\', quantity) FROM buy_sell'
+            query_res = query_db(sql)
+            parsed_query_res = '{\'transactions\': ['
 
-        sql = 'SELECT JSON_OBJECT(\'bid\', bid, \'b_type\', b_type, \'username\', username, \'t_account\', t_account, \'price\', price, \'quantity\', quantity) FROM buy_sell'
-        query_res = app.config['DB_CONN'].execute(sql).fetchall()
-        parsed_query_res = '{\'transactions\': ['
-
-        for entry in query_res:
-            parsed_query_res = parsed_query_res + entry[0] + ', '
-        parsed_query_res = parsed_query_res[:len(parsed_query_res)-2]
-        parsed_query_res += ']}'
-        transactions = json.loads(parsed_query_res.replace('\'', '\"'))
+            for entry in query_res:
+                parsed_query_res = parsed_query_res + entry[0] + ', '
+            parsed_query_res = parsed_query_res[:len(parsed_query_res)-2]
+            parsed_query_res += ']}'
+            transactions = json.loads(parsed_query_res.replace('\'', '\"'))
+            return transactions, 200
+        else:
+            return 'Only the admin may view transactions', 500
     else:
-        print('Only the admin may view banking system transactions')
-
-    return transactions, 200
+        return 'Access token is missing or invalid', 401
 
 if __name__ == "__main__":
 
